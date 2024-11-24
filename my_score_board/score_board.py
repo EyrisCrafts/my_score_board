321dec79252af6ee8feaa3e768644b73f0bfac03
Waleed <waleed.nizamaniwn@gmail.com>
2024-11-24 09:51:28 +0100

    added score_board class with tests

---
+++ my_score_board/score_board.py
@@ -0,0 +1,44 @@
+from typing import List
+from .match import Match
+
+class ScoreBoard:
+    def __init__(self):
+        self.matches: List[Match] = []
+    
+    # Start a new match
+    def start_match(self, home_team: str, away_team: str):
+        match = Match(home_team, away_team)
+        self.matches.append(match)
+    
+    def update_score(self, home_team: str, away_team: str, score_home: int, score_away: int):
+        match = self._find_match(home_team, away_team)
+        if match:
+            match.update_score(score_home, score_away)
+        else:
+            raise ValueError(f"No match found between {home_team} and {away_team}.")
+
+    
+    def finish_match(self, home_team: str, away_team: str):
+        match = self._find_match(home_team, away_team)
+        if match:
+            self.matches.remove(match)
+        else:
+            raise ValueError(f"No match found between {home_team} and {away_team}.")
+
+
+    def generate_summary(self) -> List[str]:
+        return sorted(
+            self.matches,
+            key=lambda match: (match.get_total_score(), match.match_start_time_ms),
+            reverse=True
+        )
+
+    
+    def _find_match(self, home_team: str, away_team: str) -> Match:
+        for match in self.matches:
+            if match.home_team == home_team and match.away_team == away_team:
+                return match
+        return None
+
+        
+        
---
+++ tests/test_score_board.py
@@ -0,0 +1,61 @@
+import pytest
+from my_score_board.score_board import ScoreBoard
+
+
+def test_scoreboard_start_match():
+    scoreboard = ScoreBoard()
+    scoreboard.start_match("Mexico", "Canada")
+    assert len(scoreboard.matches) == 1
+    assert scoreboard.matches[0].home_team == "Mexico"
+    assert scoreboard.matches[0].away_team == "Canada"
+
+def test_scoreboard_update_score():
+    scoreboard = ScoreBoard()
+    scoreboard.start_match("Spain", "Brazil")
+    scoreboard.update_score("Spain", "Brazil", 2, 3)
+    match = scoreboard.matches[0]
+    assert match.score_home_team == 2
+    assert match.score_away_team == 3
+    assert match.get_total_score() == 5
+
+def test_scoreboard_update_score_invalid_match():
+    scoreboard = ScoreBoard()
+    scoreboard.start_match("Spain", "Brazil")
+    with pytest.raises(ValueError, match="No match found between Germany and France."):
+        scoreboard.update_score("Germany", "France", 1, 0)
+
+def test_scoreboard_finish_match():
+    scoreboard = ScoreBoard()
+    scoreboard.start_match("Spain", "Brazil")
+    scoreboard.finish_match("Spain", "Brazil")
+    assert len(scoreboard.matches) == 0
+
+def test_scoreboard_finish_match_invalid_match():
+    scoreboard = ScoreBoard()
+    scoreboard.start_match("Argentina", "Australia")
+    with pytest.raises(ValueError, match="No match found between Mexico and Canada."):
+        scoreboard.finish_match("Mexico", "Canada")
+
+def test_scoreboard_generate_summary():
+    scoreboard = ScoreBoard()
+    scoreboard.start_match("Mexico", "Canada")
+    scoreboard.start_match("Spain", "Brazil")
+    scoreboard.start_match("Germany", "France")
+    scoreboard.start_match("Uruguay", "Italy")
+    scoreboard.start_match("Argentina", "Australia")
+
+    scoreboard.update_score("Mexico", "Canada", 0,5)
+    scoreboard.update_score("Spain", "Brazil", 10,2)
+    scoreboard.update_score("Germany", "France", 2,2)
+    scoreboard.update_score("Uruguay", "Italy", 6,6)
+    scoreboard.update_score("Argentina", "Australia", 3, 1)
+    
+    
+    summary = scoreboard.generate_summary()
+    assert summary[0].home_team == "Spain"  
+    assert summary[1].home_team == "Uruguay"
+
+def test_scoreboard_generate_summary_empty():
+    scoreboard = ScoreBoard()
+    summary = scoreboard.generate_summary()
+    assert summary == []
